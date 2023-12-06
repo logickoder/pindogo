867d09b34d3afa56f07fd9c01d28f52a0e1f56c8
Jeffery Orazulike <chukwudumebiorazulike@gmail.com>
2023-12-06 09:12:10 +0100

    fix(status-bar): minor issues

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './features/home';
+import { StatusBar, StyleSheet, View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer >
+      <StatusBar translucent />
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="home" component={HomeScreen} />
       </Stack.Navigator>
